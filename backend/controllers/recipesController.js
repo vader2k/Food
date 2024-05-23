143cbb0871cdac287d4329146d6cf5f39a2e2075
danie odukwe <kingdanny295@gmail.com>
2024-05-23 15:41:48 +0100

    @add/ added more routes to get saved recipe and recipe ids

--- backend/controllers/recipesController.js
+++ backend/controllers/recipesController.js
@@ -1,4 +1,5 @@
-import { recipeModel } from "../models/recipes.js"; 
+import { recipeModel } from "../models/recipes.js";
+import { UserModel } from "../models/Users.js"; 
 
 export const getAllRecipes = async (req,res) => {
     try {
@@ -13,8 +14,41 @@ export const createRecipe = async (req, res) => {
     const recipe = new recipeModel(req.body)
     try {
         const response = await recipe.save();
-        return res.status(200).json({success:true, message:"recipes saved", data:response})
+        return res.status(200).json({success:true, message:"recipe created", data:response})
     } catch (error) {
         return res.status(500).json({success:false, message:error.message})
+    }
+}
+
+export const saveRecipe = async (req, res) => {
+    try {
+        const recipe = await recipeModel.findById(req.body.recipeID);
+        const user = await UserModel.findById(req.body.userID);
+        user.savedRecipes.push(recipe);
+        await user.save();
+        return res.status(200).json({success:true, message:"recipe saved", data:user.savedRecipes})
+    } catch (error) {
+        return res.status(500).json({success:false, message:error.message})
+    }
+}
+
+export const savedRecipesId = async (req,res) => {
+    try {
+        const user = await UserModel.findById(req.body.userID)
+        return res.status(200).json({success:true, data:user?.savedRecipes})
+    } catch (error) {
+        return res.json(error)
+    }
+}
+
+export const savedRecipes = async (req,res) => {
+    try {
+        const user = await UserModel.findById(req.body.userID)
+        const savedRecipes = await recipeModel.find({
+            _id: {$in: user?.savedRecipes}
+        })
+        res.json(savedRecipes)
+    } catch (error) {
+        res.json(error)
     }
 }
--- backend/routes/recipesRoutes.js
+++ backend/routes/recipesRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { getAllRecipes, createRecipe } from '../controllers/recipesController.js'
+import { getAllRecipes, createRecipe, saveRecipe } from '../controllers/recipesController.js'
 
 const router = express.Router()
 
 router.get('/', getAllRecipes)
 router.post('/', createRecipe)
+router.put('/', saveRecipe)
+
 
 export {router as recipesRouter}
